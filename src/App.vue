3ac593932130742ea7fa359fb6bd8b46519a46c0
Giovanni Cappelletti <giovanni.cappelletti@yahoo.it>
2022-12-19 12:47:27 +0100

    spring spring-vue-front-end 2022-12-19

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <PizzeComp></PizzeComp>
+</template> 
+
+<script>
+import PizzeComp from './components/PizzeComp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    PizzeComp
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
